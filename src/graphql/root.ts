3ec7d8117ebe2c3081b48062027df1a3bb8f8216
Sloan <gwaltneys2@winthrop.edu>
2018-05-01 16:39:33 -0400

    added currency exchange function to GQL root

--- src/graphql/root.ts
+++ src/graphql/root.ts
@@ -1,8 +1,19 @@
-import {GraphQLObjectType} from 'graphql'
+import {GraphQLObjectType, GraphQLNonNull, GraphQLString} from 'graphql'
+import CurrencyExchangeType from './types/currency-exchange-type'
+import CurrencyExchangeResolver from './resolvers/currency-exchange-resolver'
 
 export default new GraphQLObjectType({
   name: 'Root_Type',
   fields:{
-    
+    CurrencyExchange: {
+      type: CurrencyExchangeType,
+      args: {
+        fromCountry: {type: new GraphQLNonNull(GraphQLString)},
+        toCountry: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentVal, args){
+        return CurrencyExchangeResolver(parentVal, args)
+      }
+    }
   }
 })
