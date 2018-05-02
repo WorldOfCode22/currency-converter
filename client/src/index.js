5fb24546003f8aeb97714a875fb3936f13940c76
Sloan <gwaltneys2@winthrop.edu>
2018-05-02 09:30:02 -0400

    setup of AppProvider context

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const AppContext = React.createContext()
+
+class AppProvider extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    )
+  }
+}
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>
+  , document.getElementById('root')
+)
 registerServiceWorker()
