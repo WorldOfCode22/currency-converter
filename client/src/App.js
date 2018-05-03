aaf473adf14ecf571d41a0918b170d115569a852
Sloan <gwaltneys2@winthrop.edu>
2018-05-03 14:43:32 -0400

    some convert form logic was added to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react'
+import {Container} from 'reactstrap'
 import ConversionForm from './components/conversion-form'
 
 import './App.css'
 
 const ConversionFormContext = React.createContext({})
 
-const conversionForm = {}
-
 class App extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
+    const conversionForm = {
+      fromCurrencyCode: 'USD',
+      changeFromCurrencyCode: this.changeFromCurrencyCode.bind(this),
+      fromCurrencyDropdownOpen: false,
+      fromCurrencyDropdownToggle: this.toggleFromCurrencyDropdown.bind(this),
+      toCurrencyCode: 'FKP',
+      changeToCurrencyCode: this.changeToCurrencyCode.bind(this),
+      toCurrencyDropdownOpen: false,
+      toCurrencyDropdownToggle: this.toggleToCurrencyDropdown.bind(this)
+    }
     this.state = { conversionForm }
   }
 
+  changeFromCurrencyCode (name) {
+    const {conversionForm} = this.state
+    conversionForm.fromCurrencyCode = name
+    this.setState({conversionForm})
+  }
+
+  changeToCurrencyCode (name) {
+    const {conversionForm} = this.state
+    conversionForm.toCurrencyCode = name
+    this.setState({conversionForm})
+  }
+
+  toggleToCurrencyDropdown () {
+    const {conversionForm} = this.state
+    conversionForm.toCurrencyDropdownOpen = !conversionForm.toCurrencyDropdownOpen
+    this.setState({conversionForm})
+  }
+  toggleFromCurrencyDropdown () {
+    const {conversionForm} = this.state
+    conversionForm.fromCurrencyDropdownOpen = !conversionForm.fromCurrencyDropdownOpen
+    this.setState({conversionForm})
+  }
   render () {
     return (
       <ConversionFormContext.Provider value={this.state.conversionForm}>
-        <ConversionForm />
+        <Container>
+          <ConversionForm />
+        </Container>
       </ConversionFormContext.Provider>
     )
   }
